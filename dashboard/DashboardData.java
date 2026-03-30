3e9c63d7b344e14b6e05727ff31bc8e33094d605
Eduardo-Felipe9231 <pokexgames.dudu@gmail.com>
2026-03-29 23:20:46 -0300

    Add files via upload

---
+++ dashboard/DashboardData.java
@@ -0,0 +1,21 @@
+package dashboard;
+
+public class DashboardData {
+
+    private int totalUsuarios;
+    private int totalAdmins;
+    private int totalGerentes;
+    private int totalComuns;
+
+    public int getTotalUsuarios() { return totalUsuarios; }
+    public void setTotalUsuarios(int totalUsuarios) { this.totalUsuarios = totalUsuarios; }
+
+    public int getTotalAdmins() { return totalAdmins; }
+    public void setTotalAdmins(int totalAdmins) { this.totalAdmins = totalAdmins; }
+
+    public int getTotalGerentes() { return totalGerentes; }
+    public void setTotalGerentes(int totalGerentes) { this.totalGerentes = totalGerentes; }
+
+    public int getTotalComuns() { return totalComuns; }
+    public void setTotalComuns(int totalComuns) { this.totalComuns = totalComuns; }
+}
---
+++ dashboard/DashboardService.java
@@ -0,0 +1,23 @@
+package dashboard;
+
+public class DashboardService {
+
+    private DashboardDAO dao = new DashboardDAO();
+
+    public DashboardData getDados() {
+
+        DashboardData data = new DashboardData();
+
+        try {
+            data.setTotalUsuarios(dao.contarUsuarios());
+            data.setTotalAdmins(dao.contarPorCargo(1));
+            data.setTotalGerentes(dao.contarPorCargo(2));
+            data.setTotalComuns(dao.contarPorCargo(3));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+}
