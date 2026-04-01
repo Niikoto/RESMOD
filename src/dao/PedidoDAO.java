21716336aa90fe37b8d23568086daddbb8c9696f
Niikoto <nicolasescobarsouza@gmail.com>
2026-04-01 08:46:50 -0300

    <chore>: adição mecanismo caso lista esteja vazia

--- src/dao/PedidoDAO.java
+++ src/dao/PedidoDAO.java
@@ -19,41 +19,49 @@ public class PedidoDAO {
 
         try (PreparedStatement comando = conectar.prepareStatement(sql)) {
             comando.setString(1, p.getStatus());
-            comando.setString(2,p.getMotivo());
+            comando.setString(2, p.getMotivo());
             comando.setString(3, p.getCOD_email());
 
             comando.executeUpdate();
             conectar.close();
         }
     }
-    public List<Pedido> listarPedidos(){
+
+    public List<Pedido> listarPedidos() {
         List<Pedido> pedidos = new ArrayList<>();
         Connection conectar = ConnectionFactory.getConnection();
         ResultSet resultado = null;
         String sql = "SELECT p.ID_pedido, p.motivo,p.forma_de_pagamento, p.criado, p.status, p.preço_total, p.COD_email, u.nome FROM pedido p JOIN usuario u ON p.COD_email = u.ID_email;";
 
-        try(PreparedStatement comando = conectar.prepareStatement(sql)){
+        try (PreparedStatement comando = conectar.prepareStatement(sql)) {
             resultado = comando.executeQuery();
-            while(resultado.next()){
-                Pedido pedido = new Pedido();
-                Usuario usuario = new Usuario();
-                pedido.setID_pedido(resultado.getInt(1));
-                pedido.setMotivo(resultado.getString(2));
-                pedido.setForma_de_pagamento(resultado.getString(3));
-                pedido.setCriado(resultado.getString(4));
-                pedido.setStatus(resultado.getString(5));
-                pedido.setPreco_total(resultado.getFloat(6));
-                pedido.setCOD_email(resultado.getString(7));
-                usuario.setNome(resultado.getString(8));
 
-                pedido.setUsuario(usuario);
-                pedidos.add(pedido);
+            if (!resultado.next()) {
+                return pedidos;
+            } else {
+                do {
+                    Pedido pedido = new Pedido();
+                    Usuario usuario = new Usuario();
+                    pedido.setID_pedido(resultado.getInt(1));
+                    pedido.setMotivo(resultado.getString(2));
+                    pedido.setForma_de_pagamento(resultado.getString(3));
+                    pedido.setCriado(resultado.getString(4));
+                    pedido.setStatus(resultado.getString(5));
+                    pedido.setPreco_total(resultado.getFloat(6));
+                    pedido.setCOD_email(resultado.getString(7));
+                    usuario.setNome(resultado.getString(8));
+
+                    pedido.setUsuario(usuario);
+                    pedidos.add(pedido);
+                    System.out.println("passei aqui");
+                } while (resultado.next());
             }
-            
-        } catch(SQLException e){
+
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
+        System.out.println("Aqui não tá funcionando");
         return pedidos;
 
     }
