200596f696264a68b074e58443588c9a59094bc2
Manuela Oliveira <manumanuholi87@gmail.com>
2026-05-03 10:20:03 -0300

    <feat>:Ícone adicionado e correção de português

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent; 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -9,6 +10,10 @@ public class Main extends Application{
     //Mostra/chama a tela de login para o usuario
     @Override
     public void start(Stage stage) throws Exception {
+        //Nome da janela
+        stage.setTitle("NEWE Logística Integrada - RESMOD");
+        //ícone da janela ("N" do NeweLog)
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("sources/Logo_N.png")));
         Parent root = FXMLLoader.load(
                 getClass().getResource("/view/TelaLogin.fxml")
         );
